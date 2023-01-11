91bb1b8b415fd13e034bc788d3df994a7208a8ae
Ryan Park <ryan.park@westpac.com.au>
2023-01-11 21:22:51 +1100

    blueninja init

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+
+export const Layout = ({
+  navigation,
+  settings,
+  withHeaderDivider,
+  withSignUpForm,
+  children,
+}) => {
+  return (
+    <div className="text-slate-700 bg-bgYellow">
+      <Header
+        withProfile={false}
+        withDivider={withHeaderDivider}
+        navigation={navigation}
+        settings={settings}
+      />
+      <main>{children}</main>
+      <Footer withSignUpForm={withSignUpForm} settings={settings} />
+    </div>
+  );
+};
