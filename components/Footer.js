2ccf783bc00458f6df7ab5336d06805b9046ca80
Ryan Park <ryan.park@westpac.com.au>
2023-06-22 10:08:20 +1000

    test build to fix 404

--- components/Footer.js
+++ components/Footer.js
@@ -67,7 +67,7 @@ export const Footer = ({ withSignUpForm = true, settings }) => {
       <div className="grid grid-cols-1 justify-items-center gap-24">
         {withSignUpForm && <SignUpForm settings={settings} />}
         <div className="mx-auto w-full max-w-3xl text-center text-xs font-semibold tracking-tight text-slate-500">
-          Proudly published using{" "}
+          Proudly published using
           <PrismicLink href="https://prismic.io" className="text-slate-700">
             Prismic
           </PrismicLink>
